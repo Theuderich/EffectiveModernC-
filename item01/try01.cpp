fa9e6d00cffe1a8c07ab917ffa2620422c4abea8
Marco Barthel <mabarthe@cisco.com>
2020-03-04 14:10:00 +0100

    Play with lvalue and rvalue references.

--- item01/try01.cpp
+++ item01/try01.cpp
@@ -6,7 +6,32 @@
 
 #include <iostream>
 
+
+void printInt(int& t_lValueReference)
+{
+    std::cout << "lValue Reference: " << t_lValueReference << std::endl;
+}
+
+void printInt(int&& t_rValueReference)
+{
+    std::cout << "rValue Reference: " << t_rValueReference << std::endl;
+}
+
 void testTry01()
 {
-    std::cout << "Test: Try01" << std::endl;
+    std::cout << "Test Try01: lvalue, rvalue, lvalue reference, rvalue reference" << std::endl;
+    
+    int a = 5;      // a is a simple variable
+    int& b = a;     // b is a lvalue reference;
+    //int&& c = a;    // ERROR: a as lvalue can not bind to rvalue reference
+    int&& c = a+1;    // c is a rvalue reference;
+    
+    printInt(a);    // call with lvalue
+    printInt(b);    // call with lvalue reference
+    printInt(c);    // call with rvalue reference
+
+    printInt(a+1);  // (a+1) creates a rvalue
+    printInt(2);    // 2 is a rvalue
+    
+    
 }
